41d039034d20a3e741f24d2b2c985706d9ab939f
Julia Luskina <163836018+J1411L@users.noreply.github.com>
2024-11-16 23:18:20 +0300

    основной (подключены файлы для обработки)

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from txt_processor import process_txt_file
 from xml_processor import process_xml_file
+from json_processor import process_json_file
+from yaml_processor import process_yaml_file
 import os
 
 def main():
-    file_type = input("Введите тип входного файла (txt/xml): ").strip().lower()
+    file_type = input("Введите тип входного файла (txt/xml/json/yaml): ").strip().lower()
 
     if file_type == 'txt':
         input_file = 'input.txt'
@@ -13,8 +15,16 @@ def main():
         input_file = 'input.xml'
         output_file = 'output.xml'
         process_xml_file(input_file, output_file)
+    elif file_type == 'json':
+        input_file = 'input.json'
+        output_file = 'output.json'
+        process_json_file(input_file, output_file)
+    elif file_type == 'yaml':
+        input_file = 'input.yaml'
+        output_file = 'output.yaml'
+        process_yaml_file(input_file, output_file)
     else:
-        print("Ошибка: неподдерживаемый тип файла. Пожалуйста, используйте 'txt' или 'xml'.")
+        print("Ошибка: неподдерживаемый тип файла. Пожалуйста, используйте 'txt', 'xml', 'json', 'yaml'.")
 
 if __name__ == "__main__":
     main()
