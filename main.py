3cd6bac9675c47f551b0fa1aae6d9a503065ff7f
Julia Luskina <163836018+J1411L@users.noreply.github.com>
2024-11-20 19:03:56 +0300

    + выбор

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-from txt_processor import process_txt_file
-from xml_processor import process_xml_file
-from json_processor import process_json_file
-from yaml_processor import process_yaml_file
+from mod.txt_processor import process_txt_file
+from mod.xml_processor import process_xml_file
+from mod.json_processor import process_json_file
+from mod.yaml_processor import process_yaml_file
 import os
 
 def main():
+    #print("Текущая рабочая директория:", os.getcwd())
     file_type = input("Введите тип входного файла (txt/xml/json/yaml): ").strip().lower()
+    print("Выберите метод обработки:")
+    print("1. Регулярные выражения")
+    print("2. Парсинг")
+    print("3. Математическая библиотека (eval)")
+    method = int(input("Введите номер метода (1, 2 или 3): "))
 
     if file_type == 'txt':
-        input_file = 'input.txt'
-        output_file = 'output.txt'
-        process_txt_file(input_file, output_file)
+        input_file = 'data/input.txt'
+        output_file = 'data/output.txt'
+        process_txt_file(input_file, output_file, method)
     elif file_type == 'xml':
-        input_file = 'input.xml'
-        output_file = 'output.xml'
-        process_xml_file(input_file, output_file)
+        input_file = 'data/input.xml'
+        output_file = 'data/output.xml'
+        process_xml_file(input_file, output_file, method)
     elif file_type == 'json':
-        input_file = 'input.json'
-        output_file = 'output.json'
-        process_json_file(input_file, output_file)
+        input_file = 'data/input.json'
+        output_file = 'data/output.json'
+        process_json_file(input_file, output_file, method)
     elif file_type == 'yaml':
-        input_file = 'input.yaml'
-        output_file = 'output.yaml'
-        process_yaml_file(input_file, output_file)
+        input_file = 'data/input.yaml'
+        output_file = 'data/output.yaml'
+        process_yaml_file(input_file, output_file, method)
     else:
         print("Ошибка: неподдерживаемый тип файла. Пожалуйста, используйте 'txt', 'xml', 'json', 'yaml'.")
 
