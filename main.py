d358fdac0e43b92a43220011d9e211f2245225f8
Julia Luskina <163836018+J1411L@users.noreply.github.com>
2024-12-06 00:18:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-from flask import Flask, render_template, request, jsonify  # HTML-шаблонов,
-                                                            # для работы с HTTP-запросами
-                                                            #  для формирования JSON-ответов.
-from flask import send_from_directory # для отправки файлов из определенной директории
-from werkzeug.utils import secure_filename # для безопасного имени файла
+from flask import Flask, render_template, request, jsonify
+# основная среда для веб-приложений
+# отображает HTML-шаблоны.
+# обрабатывает входящие запросы.
+# преобразует данные в формат JSON для ответов.
+from flask import send_from_directory
+# отправляет файлы из указанного каталога.
+from werkzeug.utils import secure_filename
+# очищает имена файлов, чтобы гарантировать их безопасное использование
 import os
+# взаимодействует с операционной системой для операций с файлами и каталогами
 
 from mod.txt_processor import process_txt_file
 from mod.xml_processor import process_xml_file
 from mod.json_processor import process_json_file
 from mod.yaml_processor import process_yaml_file
 
-app = Flask(__name__) # Создание экземпляра приложения
+app = Flask(__name__)
 
-# Папка для загрузки файлов
+# определяем каталоги для загружаемых файлов и обработанных выходных данных
 UPLOAD_FOLDER = 'data'
 OUTPUT_FOLDER = 'outputs'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
+# jпределяеv маршрут для загрузки обработанных файлов
 @app.route('/download/<filename>')
 def download_file(filename):
-    return send_from_directory(OUTPUT_FOLDER, filename, as_attachment=True)
+    try:
+        return send_from_directory(OUTPUT_FOLDER, filename, as_attachment=True)
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)})
+
 
 # Главная страница с формой
 @app.route('/')
@@ -31,21 +41,18 @@ def index():
 @app.route('/process', methods=['POST'])
 def process_file():
     try:
-        # Получаем файл и параметры
         uploaded_file = request.files['file']
-        #file_type = request.form['file_type']
         method = int(request.form['method'])
 
         if uploaded_file.filename == '':
             return jsonify({'status': 'error', 'message': 'Файл не выбран.'})
 
-        # Сохраняем файл
         filename = secure_filename(uploaded_file.filename)
         input_path = os.path.join(UPLOAD_FOLDER, filename)
         output_path = os.path.join(OUTPUT_FOLDER, f"processed_{filename}")
         uploaded_file.save(input_path)
 
-        file_type = filename.rsplit('.', 1)[-1].lower()  # Получаем тип файла из названия файла
+        file_type = filename.rsplit('.', 1)[-1].lower()
 
         # Определяем обработку
         if file_type == 'txt':
@@ -59,9 +66,12 @@ def process_file():
         else:
             return jsonify({'status': 'error', 'message': 'Неподдерживаемый тип файла.'})
 
-        return jsonify({'status': 'success', 'message': f'Файл успешно обработан. Скачать: {output_path}'})
+        return jsonify({'status': 'success', 'message': f'Файл успешно обработан.', 'download_url': f'/download/{os.path.basename(output_path)}'})
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)})
 
+# Запускает приложение Flask в режиме отладки, который
+# выводит подробные сообщения об ошибках и автоматически
+# перезагружает сервер при изменении кода
 if __name__ == '__main__':
     app.run(debug=True)
